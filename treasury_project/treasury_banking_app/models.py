0cee3b2c6daf02427ec78277ac8001df7fe6b438
Pawel <pawel.lisek.fp@gmail.com>
2021-06-27 15:36:21 +0200

    models created and migrated, local_settings.py corrected

--- treasury_project/treasury_banking_app/models.py
+++ treasury_project/treasury_banking_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+TEAM_CHOICE = (
+    ('treasury', 'TREASURY'),
+    ('non-treasury', 'NON-TREASURY'),
+)
+
+PERMISSION_CHOICE = (
+    ('create payment', 'CREATE PAYMENT'),
+    ('approve payment', 'APPROVE PAYMENT'),
+    ('safety administrator', 'SAFETY ADMINISTRATOR'),
+)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    surname = models.CharField(max_length=255, blank=False)
+    internal_id = models.CharField(max_length=7, blank=False)
+    team = models.CharField(max_length=255, choices=TEAM_CHOICE)
+    account = models.ManyToManyField('Account')
+
+
+class Permission(models.Model):
+    permission_type = models.CharField(max_length=255, choices=PERMISSION_CHOICE)
+    aml_required = models.BooleanField(default=False)
+    user = models.ManyToManyField(User)
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255, unique=True)
+    bank = models.ManyToManyField('Bank')
+
+
+class Bank(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Account(models.Model):
+    iban_number = models.CharField(max_length=64, unique=True)
+    swift_code = models.CharField(max_length=64)
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
