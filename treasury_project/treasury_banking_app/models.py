12d7355fe3c7f99f64ebe5bcc66d8211498c6296
Pawel <pawel.lisek.fp@gmail.com>
2021-06-27 16:09:52 +0200

    models corrected - moving choices to within the models

--- treasury_project/treasury_banking_app/models.py
+++ treasury_project/treasury_banking_app/models.py
@@ -1,34 +1,39 @@
 from django.db import models
 
-TEAM_CHOICE = (
-    ('treasury', 'TREASURY'),
-    ('non-treasury', 'NON-TREASURY'),
-)
-
-PERMISSION_CHOICE = (
-    ('create payment', 'CREATE PAYMENT'),
-    ('approve payment', 'APPROVE PAYMENT'),
-    ('safety administrator', 'SAFETY ADMINISTRATOR'),
-)
-
 
 class User(models.Model):
+    TREASURY = 'TRE'
+    NON_TREASURY = 'NTRE'
+    TEAM_CHOICE = [
+        (TREASURY, 'TREASURY'),
+        (NON_TREASURY, 'NON-TREASURY'),
+    ]
     name = models.CharField(max_length=255, blank=True, null=True)
     surname = models.CharField(max_length=255, blank=False)
     internal_id = models.CharField(max_length=7, blank=False)
-    team = models.CharField(max_length=255, choices=TEAM_CHOICE)
+    team = models.CharField(max_length=4, choices=TEAM_CHOICE)
     account = models.ManyToManyField('Account')
 
 
 class Permission(models.Model):
-    permission_type = models.CharField(max_length=255, choices=PERMISSION_CHOICE)
+    CREATE_PAYMENT = 'CR'
+    DELETE_PAYMENT = 'DP'
+    APPROVE_PAYMENT = 'AP'
+    SAFETY_ADMINISTRATOR = 'SA'
+    PERMISSION_CHOICE = [
+        (CREATE_PAYMENT, 'Create Payment'),
+        (DELETE_PAYMENT, 'Delete Payment'),
+        (APPROVE_PAYMENT, 'Approve Payment'),
+        (SAFETY_ADMINISTRATOR, 'Safety Administrator'),
+    ]
+    permission_type = models.CharField(max_length=2, choices=PERMISSION_CHOICE)
     aml_required = models.BooleanField(default=False)
     user = models.ManyToManyField(User)
 
 
 class Company(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    country = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
     bank = models.ManyToManyField('Bank')
 
 
