0b6842ee8ce189e405287345d7b89eed3b922a55
Pawel <pawel.lisek.fp@gmail.com>
2021-06-30 21:18:46 +0200

    further coding in templates, views and models

--- treasury_project/treasury_banking_app/forms.py
+++ treasury_project/treasury_banking_app/forms.py
@@ -1,18 +1,35 @@
-# from django import forms
-#
-#
-# class UserEditForm(forms.Form):
-#     CREATE_PAYMENT = 'CR'
-#     DELETE_PAYMENT = 'DP'
-#     APPROVE_PAYMENT = 'AP'
-#     PERMISSION_CHOICE = [
-#         (CREATE_PAYMENT, 'Create Payment'),
-#         (DELETE_PAYMENT, 'Delete Payment'),
-#         (APPROVE_PAYMENT, 'Approve Payment'),
-#     ]
-#     name = forms.CharField(max_length=255)
-#     surname = forms.CharField(max_length=255, blank=False)
-#     internal_id = forms.CharField(max_length=7, blank=False)
-#     permission_type = forms.CharField(max_length=2, choices=PERMISSION_CHOICE, default=CREATE_PAYMENT)
-#     account = forms.MultipleChoiceField(queryset=Account)
-#     is_administrator = models.BooleanField(default=False)
+from django.db import models
+from django import forms
+from treasury_banking_app.models import Account, PERMISSION_CHOICE, Bank, User
+
+
+class UserViewForm(forms.Form):
+    name = forms.CharField(max_length=255)
+    surname = forms.CharField(max_length=255, required=True)
+    internal_id = forms.CharField(max_length=7, required=True)
+    account = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple)
+    is_administrator = forms.BooleanField(initial=False)
+    permission = forms.MultipleChoiceField(choices=PERMISSION_CHOICE, widget=forms.CheckboxSelectMultiple)
+
+
+class AdministratorViewForm(models.Model):
+    name = forms.CharField(max_length=255, required=True)
+    surname = forms.CharField(max_length=255, required=True)
+    login = forms.CharField(max_length=255, required=True)
+    password = forms.CharField(max_length=64, widget=forms.PasswordInput)
+
+
+class CompanyViewForm(models.Model):
+    name = forms.CharField(max_length=255, unique=True)
+    country = forms.CharField(max_length=255)
+    bank = forms.MultipleChoiceField(Bank.objects.all(), widget=forms.CheckboxSelectMultiple)
+
+
+class BankViewForm(models.Model):
+    name = forms.CharField(max_length=255, unique=True)
+
+
+class AccountViewForm(models.Model):
+    class Meta:
+        model = Account
+        fields = ['iban_number', 'swift_code', 'bank', 'company']
--- treasury_project/treasury_banking_app/models.py
+++ treasury_project/treasury_banking_app/models.py
@@ -1,33 +1,20 @@
 from django.db import models
 
+PERMISSION_CHOICE = (
+    ('CREATE_PAYMENT', 'Create Payment'),
+    ('DELETE_PAYMENT', 'Delete Payment'),
+    ('APPROVE_PAYMENT', 'Approve Payment'),
+)
+
 
 class User(models.Model):
-    CREATE_PAYMENT = 'CR'
-    DELETE_PAYMENT = 'DP'
-    APPROVE_PAYMENT = 'AP'
-    PERMISSION_CHOICE = [
-        (CREATE_PAYMENT, 'Create Payment'),
-        (DELETE_PAYMENT, 'Delete Payment'),
-        (APPROVE_PAYMENT, 'Approve Payment'),
-    ]
     name = models.CharField(max_length=255, blank=True, null=True)
     surname = models.CharField(max_length=255, blank=False)
     internal_id = models.CharField(max_length=7, blank=False)
     account = models.ManyToManyField('Account')
     is_administrator = models.BooleanField(default=False)
-    permission = models.ManyToManyField('Permission')
-
-
-class Permission(models.Model):
-    CREATE_PAYMENT = 'CR'
-    DELETE_PAYMENT = 'DP'
-    APPROVE_PAYMENT = 'AP'
-    PERMISSION_CHOICE = [
-        (CREATE_PAYMENT, 'Create Payment'),
-        (DELETE_PAYMENT, 'Delete Payment'),
-        (APPROVE_PAYMENT, 'Approve Payment'),
-    ]
-    permission_type = models.CharField(max_length=2, choices=PERMISSION_CHOICE)
+    permission = models.CharField(max_length=64, choices=PERMISSION_CHOICE,
+                                  default='CREATE_PAYMENT')
 
 
 class Administrator(models.Model):
@@ -42,9 +29,15 @@ class Company(models.Model):
     country = models.CharField(max_length=255)
     bank = models.ManyToManyField('Bank')
 
+    def __str__(self):
+        return self.name
+
 
 class Bank(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Account(models.Model):
