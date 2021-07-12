28c072e977882083573d4f09fcf15b6f8acc5188
Pawel <pawel.lisek.fp@gmail.com>
2021-07-12 07:27:22 +0200

    morning work on tests

--- treasury_project/treasury_banking_app/tests/test_create_views.py
+++ treasury_project/treasury_banking_app/tests/test_create_views.py
@@ -1,7 +1,7 @@
 import pytest
 from treasury_banking_app.models import User, Company, Administrator
 
-# test to pass with all details correctly provided
+
 @pytest.mark.django_db
 def test_user_create_view(client):
     name = 'Katarzyna'
@@ -21,29 +21,10 @@ def test_user_create_view(client):
     assert response.status_code == 302
     assert User.objects.get(name=name)
 
-# test to fail with all missing name
+
 @pytest.mark.django_db
-def test_user_create_view(client):
-    name = ''
-    surname = 'Wolska'
-    internal_id = 'PKY8966'
-    is_administrator = True
-    is_payment_creator = True
-    is_payment_approver = False
-    can_delete_payment = True
-    response = client.post('/user_create/', {'name': name,
-                                             'surname': surname,
-                                             'internal_id': internal_id,
-                                             'is_administrator': is_administrator,
-                                             'is_payment_creator': is_payment_creator,
-                                             'is_payment_approver': is_payment_approver,
-                                             'can_delete_payment': can_delete_payment})
-    assert response.status_code == 302
-    assert User.objects.get(name=name)
-
-
-
-
+def test_user_model(user):
+    assert User.objects.get(name='Janek') == user
 
 
 @pytest.mark.django_db
@@ -56,16 +37,25 @@ def test_company_create_view(client):
     assert Company.objects.get(name=name)
 
 
-# test to fail as the login is too long
+@pytest.mark.django_db
+def test_company_model(company):
+    assert Company.objects.get(name='Tre G.M.B.H.') == company
+
+
 @pytest.mark.django_db
 def test_administrator_create_view(client):
-    name = 'Bogdan'
-    surname = 'Suchy'
-    login = 'PLIjds70'
-    password = 'Password_1'
-    password_repeat = 'Password_1'
+    name = 'Jacek'
+    surname = 'Kruchy'
+    login = 'PLIjds7'
+    password = 'Password_2'
+    password_repeat = 'Password_2'
     response = client.post('/admin_create/', {'name': name, 'surname': surname, 'login': login,
                                               'password': password, 'password_repeat': password_repeat})
 
     assert response.status_code == 302
     assert Administrator.objects.get(name=name)
+
+
+@pytest.mark.django_db
+def test_administrator_model(administrator):
+    assert Administrator.objects.get(surname='Paluch') == administrator
